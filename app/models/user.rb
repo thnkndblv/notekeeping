d000101b34c825cf995b9578fa98708c7b4e6c41
Manuel Morales <manuel@kueski.com>
2018-02-04 21:33:32 -0600

    Add password constraints to user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -12,6 +12,10 @@ class User < ApplicationRecord
     length: { maximum: 50 },
     format: { with: EMAIL_REGEX },
     uniqueness: { case_sensitive: false }
-  
+
+  validates :password,
+    presence: true,
+    length: { minimum: 6 }
+
   has_secure_password
 end
