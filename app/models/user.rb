1eade803c99af20f38ab1873aeee76628f8722f7
Manuel Morales <manuel@kueski.com>
2018-02-05 13:32:47 -0600

    Add a digest method to user model to help on integration test

--- app/models/user.rb
+++ app/models/user.rb
@@ -18,4 +18,9 @@ class User < ApplicationRecord
     length: { minimum: 6 }
 
   has_secure_password
+
+  def self.digest(string)
+    cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST : BCrypt::Engine.cost
+    BCrypt::Password.create(string, cost: cost)
+  end
 end
