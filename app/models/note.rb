a12b4da03049c4c97612a6a20b7cdaaae9896387
Manuel Morales <manuel.cpp@gmail.com>
2018-02-08 03:21:50 -0600

    Add helper scopes to models not and share

--- app/models/note.rb
+++ app/models/note.rb
@@ -5,4 +5,6 @@ class Note < ApplicationRecord
   validates :user_id, presence: true
   validates :title, presence: true, length: { maximum: 100 }
   validates :content, presence: true
+
+  scope :actives, -> { where(active: true) }
 end
